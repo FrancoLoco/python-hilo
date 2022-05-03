d9622f6b529d52ba72856b99b8669567f6684794
francispoisson <francis.poisson2@gmail.com>
2022-05-03 09:56:43 -0400

    Update test.py
    Juste pour tester

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import asyncio
 from aiohttp import ClientSession
 import logging
 from os import environ
-from pyhilo import API, Hilo
-from pyhilo.const import LOG  # noqa
+import pyhilo
+from pyhilo import API
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -13,21 +13,10 @@ logging.basicConfig(
 
 
 async def async_main() -> None:
-    # export hilo_username="moi@gmail.com"
-    # export hilo_password="1234"
-    username = environ.get("hilo_username", "")
-    password = environ.get("hilo_password", "")
-    api = await API.async_auth(username, password, session=ClientSession())
-    h = Hilo(api)
-    await h.async_init()
-
-    while True:
-        for d in h.devices:
-            print(d.name) #example to list devices and attributes
-            print(d.get_attribute('Power'))
-            print(d.get_attribute('CurrentTemperature'))
-            print(d.get_attribute('OnOff'))
-        await asyncio.sleep(2)
+    username="x@gmail.com"
+    password="yyyyyy"
+    api = await API.async_auth_password(username, password, session=ClientSession())
+    print(api.device_attributes)
 
 
 loop = asyncio.get_event_loop()
